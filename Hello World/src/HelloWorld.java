bb87e552bac3201e715081c8075457fe09734210
jonathan.poister <jonathan.poister@MU-C1-08.mullen.pvt.k12.co.us>
2016-11-07 09:00:04 -0700

    final

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	String text = "Hello";
+		
+		System.out.println("Hello World!");
+		Scanner userInput = new Scanner(System.in);
+		
+		
+		
+		
+		}
+
+}
